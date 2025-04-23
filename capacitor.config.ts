1c42e6ee3c742dc13b26e367a4a48b43c6af0e37
Padmaj20 <135749129+Padmaj20@users.noreply.github.com>
2025-04-23 18:17:27 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fitfuell.app',
+  appName: 'fitfuell',
+  webDir: 'dist'
+};
+
+export default config;
